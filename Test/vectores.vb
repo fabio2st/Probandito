3525d17c9bbe63ffd3f4da8a4b1341bdd889675b
Fabio Dos Santos <fabio2st@gmail.com>
2017-05-15 18:43:03 -0300

    matriz

--- Test/vectores.vb
+++ Test/vectores.vb
@@ -1,5 +1,32 @@
 ﻿Module vectores
     Sub main()
+        Dim numbers = New Integer() {1, 2, 4, 8}
+        For x = 0 To numbers.Length - 1
+            numbers(x) = numbers(x) * 2
+        Next
+        For Each number As Integer In numbers
+            Console.WriteLine(number)
+        Next
+
+        Dim matrix = New Integer(5, 3) {{1, 2, 9}, {3, 4}, {5, 6}, {7, 8}}
+
+        For x = 0 To matrix.GetLength(0) - 1
+            For y = 0 To matrix.Rank - 1
+                Console.WriteLine(matrix(x, y))
+            Next
+        Next
+
+        For y = 0 To matrix.Rank - 1
+            For x = 0 To matrix.GetLength(1) - 1
+                Console.WriteLine(matrix(x, y))
+            Next
+        Next
+
+        For Each number As Integer In matrix
+            Console.WriteLine(number)
+        Next
+
+        Console.ReadKey()
 
     End Sub
 End Module
